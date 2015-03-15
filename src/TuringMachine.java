9fdbf3bf8c69e76d8b4d094a4182b36ab73ac06f
Johannes Bucher <info@johannes-bucher.de>
2015-03-15 14:46:23 +0100

    alternate TuringMachine constructor with arrays instead of ArrayLists

--- src/TuringMachine.java
+++ src/TuringMachine.java
@@ -16,6 +16,15 @@ public class TuringMachine {
 		terminated = false;
 	}
 	
+	public TuringMachine(String[] initialBand, String[] alphabet, int numberOfStates, int startState) {
+		ArrayList<String> a = new ArrayList<String>(Arrays.asList(alphabet));
+		ArrayList<String> ini = new ArrayList<String>(Arrays.asList(initialBand));
+		band = new TuringBand(ini);
+		states = new TuringState(a, numberOfStates);
+		currentState = startState;
+		terminated = false;
+	}
+	
 	public void addStateFunction(int state, String[] write, Integer[] dir, Integer[] newStates) {
 		ArrayList<String> w = new ArrayList<String>(Arrays.asList(write));
 		ArrayList<Integer> d = new ArrayList<Integer>(Arrays.asList(dir));
@@ -37,7 +46,9 @@ public class TuringMachine {
 	}
 	
 	public String printBand() {
-		return band.printBand();
+		String str = band.printBand();
+		str = str.replaceAll("\\[HEAD\\]", "s" + Integer.toString(currentState));
+		return str;
 	}
 	
 	public boolean isTerminated() {
